dea84a8cb8f7c4eb5be7e780ada5ef39682ab61e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-06 18:59:51 +0100

    Update scanFolders.py

--- scanFolders.py
+++ scanFolders.py
@@ -116,6 +116,7 @@ def scanFolder(theInput, theOutput):
                     stderrOutput = commandLineResult.stderr.strip()
                     if not stderrOutput == "":
                         print(stderrOutput)
+                        sys.exit(0)
         if (matched == False) and (folderMatched == False) and (not item == ""):
             unmatchedItems.append(item)
     for item in unmatchedItems:
