5a432492c9b2a53e00cddba075633b959b5695a1
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-06 11:09:34 +0100

    Add files via upload

--- scanFolders.sh
+++ scanFolders.sh
@@ -4,6 +4,10 @@ if [ ! -d .venv ]; then
   python -m venv .venv
   source .venv/bin/activate
   pip install -r requirements.txt
+else
+  source .venv/bin/activate
 fi
 
-echo Hello!...
+cp docsToMarkdownLib.py .venv/lib/python3.13/site-packages
+
+python3 scanFolders.py "$@"
