1fd4a40ae2422d13a96a22ec8b5c8423615f5c4f
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-06 11:31:22 +0100

    Update processFAQ.py

--- FAQ/processFAQ.py
+++ FAQ/processFAQ.py
@@ -3,7 +3,7 @@ import sys
 import subprocess
 
 # Our own Docs To Markdown library.
-import docsToMarkdownLib
+import officeToMarkdownLib
 
 # Get the command line arguments.
 inputFolder = sys.argv[1]
@@ -14,12 +14,12 @@ for inputItem in os.listdir(inputFolder):
     fileType = inputItem.rsplit(".", 1)[1].upper()
     if fileType in ["DOCX", "DOC"]:
         # Deal with a DOCX / DOC file - pass it up to the processDOCFile script to deal with.
-        subprocess.run(["python3", "../docs-to-markdown/processDOCFile.py", docsToMarkdownLib.normalisePath(inputFolder + "/" + inputItem), docsToMarkdownLib.normalisePath(outputFolder)])
+        subprocess.run(["python3", "../docs-to-markdown/processDOCFile.py", officeToMarkdownLib.normalisePath(inputFolder + "/" + inputItem), officeToMarkdownLib.normalisePath(outputFolder)])
     elif fileType in ["MP4"]:
         # Deal with an MP4 file - use FFmpeg to set the size and format of any videos in this FAQ.
         outputItem = inputItem.rsplit(".", 1)[0] + ".webm"
         # Video files can take time / processing power to deal with, so we check we actually need to update something first before going ahead.
-        if not docsToMarkdownLib.checkModDatesMatch(inputFolder + os.sep + inputItem, outputFolder + os.sep + outputItem):
+        if not officeToMarkdownLib.checkModDatesMatch(inputFolder + os.sep + inputItem, outputFolder + os.sep + outputItem):
             print("STATUS: Processing FAQ video: " + inputFolder + os.sep + inputItem + " to " + outputFolder + os.sep + outputItem, flush=True)
             
             # Figure out the video's dimensions.
@@ -32,4 +32,4 @@ for inputItem in os.listdir(inputFolder):
             os.system("ffmpeg -i " + inputFolder + os.sep + inputItem + " -filter:v crop=" + str(videoHeight) + ":" + str(videoHeight) + ":" + str(int((videoWidth - videoHeight) / 2)) + ":0,scale=240:240,setsar=1 -filter:a loudnorm=I=-16:LRA=11:TP=-1.5 /tmp/faq.webm > /dev/null 2>&1")
             os.system("mv /tmp/faq.webm " + outputFolder + os.sep + outputItem)
             
-            docsToMarkdownLib.makeModDatesMatch(inputFolder + os.sep + inputItem, outputFolder + os.sep + outputItem)
+            officeToMarkdownLib.makeModDatesMatch(inputFolder + os.sep + inputItem, outputFolder + os.sep + outputItem)
