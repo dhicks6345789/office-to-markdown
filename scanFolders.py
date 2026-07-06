392fc97b92d4cdb9a523488a49802d05edff25ea
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-06 11:22:53 +0100

    Update scanFolders.py

--- scanFolders.py
+++ scanFolders.py
@@ -12,29 +12,29 @@ import officeToMarkdownLib
 
 
 # Parse and normalise the command-line arguments.
-args = docsToMarkdownLib.processCommandLineArgs(defaultArgs={"scriptRoot":str(pathlib.Path.cwd()), "dataRoot":str(pathlib.Path.cwd()), "verbose":"false", "produceFolderIndexes":"false", "deleteExtraFiles":"false", "validFrontMatterFields":""}, requiredArgs=["input","output"], optionalArgs=["scriptRoot", "verbose", "deleteExtraFiles", "copyIn", "data", "produceFolderIndexes", "baseURL", "validFrontMatterFields"])
-args["dataRoot"] = docsToMarkdownLib.normalisePath(args["dataRoot"])
+args = officeToMarkdownLib.processCommandLineArgs(defaultArgs={"scriptRoot":str(pathlib.Path.cwd()), "dataRoot":str(pathlib.Path.cwd()), "verbose":"false", "produceFolderIndexes":"false", "deleteExtraFiles":"false", "validFrontMatterFields":""}, requiredArgs=["input","output"], optionalArgs=["scriptRoot", "verbose", "deleteExtraFiles", "copyIn", "data", "produceFolderIndexes", "baseURL", "validFrontMatterFields"])
+args["dataRoot"] = officeToMarkdownLib.normalisePath(args["dataRoot"])
 args["verbose"] = args["verbose"].lower()
 args["deleteExtraFiles"] = args["deleteExtraFiles"].lower()
 args["produceFolderIndexes"] = args["produceFolderIndexes"].lower()
 args["validFrontMatterFields"] = args["validFrontMatterFields"].split(",")
 
 # Print a config summary for the user.
-print("DocsToMarkdown - arguments:", flush=True)
+print("OfficeToMarkdown - arguments:", flush=True)
 for arg in args:
     print(" - " + arg + ": " + str(args[arg]), flush=True)
 
 # Read the "matches.csv" file, which describes which transform script to run for each file type / sub folder in the input folder structure.
-matches = docsToMarkdownLib.readDataFile(args["dataRoot"] + os.sep + "matches.csv")
+matches = officeToMarkdownLib.readDataFile(args["dataRoot"] + os.sep + "matches.csv")
 scriptStrings = []
 for item in matches:
-    scriptString = docsToMarkdownLib.normalisePath(args["scriptRoot"] + "/" + matches[item][1])
+    scriptString = officeToMarkdownLib.normalisePath(args["scriptRoot"] + "/" + matches[item][1])
     if not scriptString in scriptStrings:
         scriptStrings.append(scriptString)
 
 # Read the matchChanges cache file, and work out if any of the transform scripts have been updated since the last run.
-previousMatchChanges = docsToMarkdownLib.readDataFile(args["dataRoot"] + os.sep + "matchChanges.csv")
-currentMatchChanges = docsToMarkdownLib.getFolderChangeDetails(args["scriptRoot"])
+previousMatchChanges = officeToMarkdownLib.readDataFile(args["dataRoot"] + os.sep + "matchChanges.csv")
+currentMatchChanges = officeToMarkdownLib.getFolderChangeDetails(args["scriptRoot"])
 changedMatchPaths = []
 for item in currentMatchChanges:
     if item in scriptStrings:
@@ -46,10 +46,10 @@ for item in currentMatchChanges:
 if args["verbose"] == "true":
     print("changedMatchPaths:")
     print(changedMatchPaths)
-docsToMarkdownLib.writeDataFile(args["dataRoot"] + os.sep + "matchChanges.csv", currentMatchChanges)
+officeToMarkdownLib.writeDataFile(args["dataRoot"] + os.sep + "matchChanges.csv", currentMatchChanges)
 
-previousInputChanges = docsToMarkdownLib.readDataFile(args["dataRoot"] + os.sep + "inputChanges.csv")
-currentInputChanges = docsToMarkdownLib.getFolderChangeDetails(docsToMarkdownLib.normalisePath(args["input"]))
+previousInputChanges = officeToMarkdownLib.readDataFile(args["dataRoot"] + os.sep + "inputChanges.csv")
+currentInputChanges = officeToMarkdownLib.getFolderChangeDetails(officeToMarkdownLib.normalisePath(args["input"]))
 changedInputPaths = []
 for item in currentInputChanges:
     if item in previousInputChanges:
@@ -60,7 +60,7 @@ for item in currentInputChanges:
 if args["verbose"] == "true":
     print("changedInputPaths:")
     print(changedInputPaths)
-docsToMarkdownLib.writeDataFile(args["dataRoot"] + os.sep + "inputChanges.csv", currentInputChanges)
+officeToMarkdownLib.writeDataFile(args["dataRoot"] + os.sep + "inputChanges.csv", currentInputChanges)
 
 
 
@@ -68,9 +68,9 @@ docsToMarkdownLib.writeDataFile(args["dataRoot"] + os.sep + "inputChanges.csv", 
 # A cache of file paths with checksum details is maintained, this is used to avoid processing a file if it (and the associated processing script) hasn't been changed since the last run.
 # Folders are recursed into. Some matches might match whole sub-folders, in which case that sub-folder's processing will be handled by the transform script.
 def scanFolder(theInput, theOutput):
-    inputFolder = docsToMarkdownLib.normalisePath(args["input"] + "/" + theInput)
+    inputFolder = officeToMarkdownLib.normalisePath(args["input"] + "/" + theInput)
     if args["verbose"] == "true":
-        print("DocsToMarkdown - scanning folder: " + inputFolder, flush=True)
+        print("OfficeToMarkdown - scanning folder: " + inputFolder, flush=True)
     unmatchedItems = []
 
     items = os.listdir(inputFolder)
@@ -82,15 +82,15 @@ def scanFolder(theInput, theOutput):
             inputItem = inputFolder + "/" + item
             if (matched == False) and (folderMatched == False) and (not re.match(match, inputItem) == None):
                 matched = True
-                scriptExec = docsToMarkdownLib.platformPath(matches[match][0])
-                scriptPath = docsToMarkdownLib.platformPath(args["scriptRoot"] + "/" + matches[match][1])
-                inputItem = docsToMarkdownLib.platformPath(inputItem)
+                scriptExec = officeToMarkdownLib.platformPath(matches[match][0])
+                scriptPath = officeToMarkdownLib.platformPath(args["scriptRoot"] + "/" + matches[match][1])
+                inputItem = officeToMarkdownLib.platformPath(inputItem)
                 if item == "":
                     folderMatched = True
-                outputItem = docsToMarkdownLib.normalisePath(args["output"] + "/" + theOutput + "/" + item)
+                outputItem = officeToMarkdownLib.normalisePath(args["output"] + "/" + theOutput + "/" + item)
                 if os.path.isfile(inputItem):
                     outputItem = outputItem.rsplit("/", 1)[0]
-                outputItem = docsToMarkdownLib.platformPath(outputItem)
+                outputItem = officeToMarkdownLib.platformPath(outputItem)
                 
                 if args["verbose"] == "true":
                     print("OfficeToMarkdown - matched: " + inputItem + " with " + match, flush=True)
@@ -98,25 +98,25 @@ def scanFolder(theInput, theOutput):
                 #if scriptPath in changedMatchPaths or inputItem in changedInputPaths:
                 commandLine = [scriptExec, scriptPath, inputItem, outputItem]
                 if args["verbose"] == "true":
-                    print("DocsToMarkdown - running: " + " ".join(commandLine), flush=True)
+                    print("OfficeToMarkdown - running: " + " ".join(commandLine), flush=True)
                 subprocess.run(commandLine)
         if (matched == False) and (folderMatched == False) and (not item == ""):
             unmatchedItems.append(item)
     for item in unmatchedItems:
         if os.path.isdir(inputFolder + os.sep + item):
-            scanFolder(docsToMarkdownLib.normalisePath(theInput + os.sep + item), docsToMarkdownLib.normalisePath(theOutput + os.sep + item))
+            scanFolder(officeToMarkdownLib.normalisePath(theInput + os.sep + item), officeToMarkdownLib.normalisePath(theOutput + os.sep + item))
 
 def copyFolder(inputFolder, outputFolder):
     print("copyFolder: " + inputFolder + " -> " + outputFolder)
     for item in os.listdir(inputFolder):
         inputItem = inputFolder + "/" + item
         outputItem = outputFolder + "/" + item
-        docsToMarkdownLib.addToWriteLog(outputItem)
+        officeToMarkdownLib.addToWriteLog(outputItem)
         if os.path.isfile(inputItem):
-            if not docsToMarkdownLib.checkModDatesMatch(inputItem, outputItem):
+            if not officeToMarkdownLib.checkModDatesMatch(inputItem, outputItem):
                 print("Copying file: " + inputItem + " to " + outputItem, flush=True)
                 shutil.copyfile(inputItem, outputItem)
-                docsToMarkdownLib.makeModDatesMatch(inputItem, outputItem)
+                officeToMarkdownLib.makeModDatesMatch(inputItem, outputItem)
         else:
             os.mkdir(outputItem)
             copyFolder(inputItem, outputItem)
@@ -137,11 +137,11 @@ scanFolder("", "")
 # If the user has specified a "copy in" folder, copy the contens of that folder over to the destination as well.
 # This happens after "scan folders", so for any conflicting filenames, the copy process will take precedence.
 if "copyIn" in args and not args["copyIn"] == "":
-    copyFolder(docsToMarkdownLib.normalisePath(args["copyIn"]), docsToMarkdownLib.normalisePath(args["output"]))
+    copyFolder(officeToMarkdownLib.normalisePath(args["copyIn"]), officeToMarkdownLib.normalisePath(args["output"]))
 
-if args["deleteExtraFiles"] == "true" and os.path.isfile("/tmp/docsToMarkdownWriteLog.txt"):
-    with open("/tmp/docsToMarkdownWriteLog.txt", "r", encoding="utf-8") as writeLogFile:
-        deleteExtraFiles(docsToMarkdownLib.normalisePath(args["output"]), writeLogFile.readlines())
+if args["deleteExtraFiles"] == "true" and os.path.isfile("/tmp/officeToMarkdownWriteLog.txt"):
+    with open("/tmp/officeToMarkdownWriteLog.txt", "r", encoding="utf-8") as writeLogFile:
+        deleteExtraFiles(officeToMarkdownLib.normalisePath(args["output"]), writeLogFile.readlines())
 
 # Clear out the write log file.
-#os.remove("/tmp/docsToMarkdownWriteLog.txt")
+os.remove("/tmp/officeToMarkdownWriteLog.txt")
