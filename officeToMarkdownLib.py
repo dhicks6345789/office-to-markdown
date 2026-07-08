93acd5ba2aa77f231aa97b65ecda0558cd00791a
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-08 11:18:02 +0100

    Update officeToMarkdownLib.py

--- officeToMarkdownLib.py
+++ officeToMarkdownLib.py
@@ -414,7 +414,7 @@ def addToWriteLog(theFilename, theLogLocation="/tmp/docsToMarkdownWriteLog.txt")
         file.write(theFilename + "\n")
 
 def checkTimestampsMatch(theTimestamp, thePath):
-    print("checkTimestampsMatch: " + theTimestamp + ", " + str(thePath) + "...", flush=True, file=sys.stderr)
+    print("checkTimestampsMatch: " + theTimestamp + ", " + str(thePath) + "(" + str(thePath.stat().st_mtime) + ")" + "...", flush=True, file=sys.stderr)
     if thePath.stat().st_mtime == theTimestamp:
         print("...yes, they match.", flush=True, file=sys.stderr)
         return True
