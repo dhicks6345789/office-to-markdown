b23ae3a250aab8e9c333ed8d524542f1888da326
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-08 21:27:45 +0100

    Update processDOCFile.py

--- processDOCFile.py
+++ processDOCFile.py
@@ -22,7 +22,7 @@ for line in sys.stdin:
   lineSplit = line.strip().split(",")
   previousInputFileTimestamps[lineSplit[0]] = lineSplit[1]
 
-print("processDOCFile   - previousInputFileTimestamps: " + str(previousInputFileTimestamps), flush=True, file=sys.stderr)
+#print("processDOCFile   - previousInputFileTimestamps: " + str(previousInputFileTimestamps), flush=True, file=sys.stderr)
 
 # Check we are trying to convert a DOCX / DOC file.
 if inputPath.suffix.lower() in [".docx", ".doc"]:
