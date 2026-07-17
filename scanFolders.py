b6cb31f419a3f6a0cc92ce56ca5337077fd9a090
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-17 16:21:20 +0100

    Update scanFolders.py

--- scanFolders.py
+++ scanFolders.py
@@ -34,61 +34,58 @@ for item in matches:
     if not scriptString in scriptStrings:
         scriptStrings.append(scriptString)
 
-# Read the matchChanges cache file and get the current last-updated timestamps of the script files so we can work out if any of the transform scripts have been updated since the last run.
+# Read the matchChanges cache file, a list of the script files used to do the transforms along with their last-updated timestamps...
 previousMatchChanges = officeToMarkdownLib.readChangesFile(args["dataRoot"] + os.sep + "matchChanges.csv")
+# ...then get the current last-updated timestamps of the script files so we can work out if any of the transform scripts have been updated since the last run...
 currentMatchChanges = officeToMarkdownLib.getFolderChangeDetails(args["scriptRoot"])
+# ...and save those new update timestamps.
+officeToMarkdownLib.writeChangesFile(args["dataRoot"] + os.sep + "matchChanges.csv", currentMatchChanges)
 
 # If the main scripts or library have been updated we want all scripts to be re-run.
 for item in ["scanFolders.py", "officeToMarkdownLib.py"]:
     itemPathStr = args["scriptRoot"] + os.sep + item
     if (not itemPathStr in previousMatchChanges) or (not previousMatchChanges[itemPathStr] == currentMatchChanges[itemPathStr]):
-        if args["verbose"] == "true":
-            print(itemPathStr + " updated - re-running all scripts.")
+        officeToMarkdownLib.ifVerbose(verbose, itemPathStr + " updated - re-running all scripts.")
         previousMatchChanges = {}
-officeToMarkdownLib.writeChangesFile(args["dataRoot"] + os.sep + "matchChanges.csv", currentMatchChanges)
 
-if args["verbose"] == "true":
-    print("matches:")
-    print(matches)
-
+# Read the inputChanges cache file, a list of previously-seen input files and their last-updated filestamps.
 previousInputChanges = officeToMarkdownLib.readChangesFile(args["dataRoot"] + os.sep + "inputChanges.csv")
 
-# Start the scanFolders process.
+# Start the scanFolders process. This scans the given folder, using the passed-in dicts of last-updated timestamps to spot any changed files so we can avoid re-doing work if we don't need to.
+# The function returns a dict of updated last-updated timestamps for input files and a list of output files.
 currentInputChanges, outputFiles = officeToMarkdownLib.scanFolder(verbose, args["scriptRoot"], matches, previousMatchChanges, previousInputChanges, pathlib.Path(args["input"]), pathlib.Path(args["output"]))
 
-# Write the updated input file modification timestamps the the "inputChanges" file.
+# Write the updated last-updated file timestamps the the "inputChanges" file.
 officeToMarkdownLib.writeChangesFile(args["dataRoot"] + os.sep + "inputChanges.csv", currentInputChanges)
 
+# If the user has specified a "copy in" folder, copy the contens of that folder over to the destination as well.
+# This happens after "scan folders", so for any conflicting filenames, the copy process will take precedence.
+# This is an inline function which adds files copied to the "outputFiles" list.
 def copyFolder(inputFolder, outputFolder):
     for inputItem in inputFolder.iterdir():
         outputItem = outputFolder / item
         outputFiles.append(str(outputItem))
         if inputItem.is_file():
             if not inputItem.stat().st_mtime == outputItem.stat().st_mtime:
-                if verbose:
-                    print("Copying file: " + inputItem + " to " + outputItem, flush=True)
+                officeToMarkdownLib.ifVerbose(verbose, "Copying file: " + inputItem + " to " + outputItem, flush=True)
                 shutil.copyfile(str(inputItem), str(outputItem))
                 officeToMarkdownLib.makeModDatesMatch(str(inputItem), str(outputItem))
         else:
             os.mkdir(str(outputItem))
             copyFolder(inputItem, outputItem)
-
-# If the user has specified a "copy in" folder, copy the contens of that folder over to the destination as well.
-# This happens after "scan folders", so for any conflicting filenames, the copy process will take precedence.
 if "copyIn" in args and not args["copyIn"] == "":
     copyFolder(pathlib.Path(args["copyIn"]), pathlib.Path(args["output"]))
 
+# If the user has specified the "deleteExtraFiles" option then we clear any extra files out of the output destination. We define "extra files" as any files that could not have been produced as output files
+# this run, i.e. the contents of the "outputFiles" list. Note that this list should include files that would have been oputput by any sub-script, even if they weren't updated this run because non of their inputs
+# were updated. Sub-scripts should always return a list of all output file paths even if they don't actually output that file on a particular run.
 def deleteExtraFiles(theFolder):
     for item in theFolder.iterdir():
         if item.is_file():
             if not str(item) in outputFiles:
-                print("Removing extra file: " + str(item), flush=True)
+                officeToMarkdownLib.ifVerbose(verbose, "Removing extra file: " + str(item), flush=True)
                 os.remove(str(item))
         else:
             deleteExtraFiles(item)
-
 if args["deleteExtraFiles"] == "true":
-    #if verbose:
-        #print("Deleting extra files - valid output files:")
-        #print(outputFiles)
     deleteExtraFiles(pathlib.Path(args["output"]))
