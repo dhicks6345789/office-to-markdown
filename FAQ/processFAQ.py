9814da77871ddc8438b4aa7fb5833f6419b36830
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-17 14:36:32 +0100

    Update processFAQ.py

--- FAQ/processFAQ.py
+++ FAQ/processFAQ.py
@@ -29,7 +29,8 @@ for inputItem in inputPath.iterdir():
     outputFilePath = outputPath / pathlib.Path(inputItem.stem + ".md")
     outputFilePaths.append(outputFilePath)
     doTransform = False
-    print(previousInputChanges[str(inputItem)] + " == " + currentInputChanges[str(inputItem)], flush=True, file=sys.stderr)
+    if str(inputItem) in previousInputChanges):
+      print(previousInputChanges[str(inputItem)] + " == " + currentInputChanges[str(inputItem)], flush=True, file=sys.stderr)
     if not officeToMarkdownLib.checkTimestampsMatch(args["scriptTimestamp"], pathlib.Path(__file__)):
       doTransform = True
     elif (not str(inputItem) in previousInputChanges) or (not previousInputChanges[str(inputItem)] == currentInputChanges[str(inputItem)]):
