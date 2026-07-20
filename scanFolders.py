05a04d691fc6410a6644c50b0586053b6ae85aa6
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-20 10:20:18 +0100

    Update scanFolders.py

--- scanFolders.py
+++ scanFolders.py
@@ -13,7 +13,7 @@ import officeToMarkdownLib
 
 # Parse and normalise the command-line arguments.
 args = officeToMarkdownLib.processCommandLineArgs(defaultArgs={"scriptRoot":str(pathlib.Path.cwd()), "dataRoot":str(pathlib.Path.cwd()), "verbose":"false", "produceFolderIndexes":"false", "deleteExtraFiles":"false", "validFrontMatterFields":""}, requiredArgs=["input","output"], optionalArgs=["scriptRoot", "verbose", "deleteExtraFiles", "copyIn", "data", "produceFolderIndexes", "baseURL", "validFrontMatterFields"])
-args["dataRoot"] = officeToMarkdownLib.normalisePath(args["dataRoot"])
+dataRoot = pathlib.Path(args["dataRoot"])
 verbose = False
 if args["verbose"].lower() == "true":
     verbose = True
@@ -27,7 +27,7 @@ for arg in args:
     print(" - " + arg + ": " + str(args[arg]), flush=True)
 
 # Read the "matches.csv" file, which describes which transform script to run for each file type / sub folder in the input folder structure.
-matches = officeToMarkdownLib.readDataFile(args["dataRoot"] + os.sep + "matches.csv")
+matches = officeToMarkdownLib.readDataFile(dataRoot + os.sep + "matches.csv")
 scriptStrings = []
 for item in matches:
     scriptString = officeToMarkdownLib.normalisePath(args["scriptRoot"] + "/" + matches[item][1])
@@ -35,11 +35,11 @@ for item in matches:
         scriptStrings.append(scriptString)
 
 # Read the matchChanges cache file, a list of the script files used to do the transforms along with their last-updated timestamps...
-previousMatchChanges = officeToMarkdownLib.readChangesFile(args["dataRoot"] + os.sep + "matchChanges.csv")
+previousMatchChanges = officeToMarkdownLib.readChangesFile(dataRoot + os.sep + "matchChanges.csv")
 # ...then get the current last-updated timestamps of the script files so we can work out if any of the transform scripts have been updated since the last run...
 currentMatchChanges = officeToMarkdownLib.getFolderChangeDetails(args["scriptRoot"])
 # ...and save those new update timestamps.
-officeToMarkdownLib.writeChangesFile(args["dataRoot"] + os.sep + "matchChanges.csv", currentMatchChanges)
+officeToMarkdownLib.writeChangesFile(dataRoot + os.sep + "matchChanges.csv", currentMatchChanges)
 
 # If the main scripts or library have been updated we want all scripts to be re-run.
 for item in ["scanFolders.py", "officeToMarkdownLib.py"]:
@@ -49,14 +49,14 @@ for item in ["scanFolders.py", "officeToMarkdownLib.py"]:
         previousMatchChanges = {}
 
 # Read the inputChanges cache file, a list of previously-seen input files and their last-updated filestamps.
-previousInputChanges = officeToMarkdownLib.readChangesFile(args["dataRoot"] + os.sep + "inputChanges.csv")
+previousInputChanges = officeToMarkdownLib.readChangesFile(dataRoot + os.sep + "inputChanges.csv")
 
 # Start the scanFolders process. This scans the given folder, using the passed-in dicts of last-updated timestamps to spot any changed files so we can avoid re-doing work if we don't need to.
 # The function returns a dict of updated last-updated timestamps for input files and a list of output files.
 currentInputChanges, outputFiles = officeToMarkdownLib.scanFolder(verbose, args["scriptRoot"], matches, previousMatchChanges, previousInputChanges, pathlib.Path(args["input"]), pathlib.Path(args["output"]))
 
 # Write the updated last-updated file timestamps the the "inputChanges" file.
-officeToMarkdownLib.writeChangesFile(args["dataRoot"] + os.sep + "inputChanges.csv", currentInputChanges)
+officeToMarkdownLib.writeChangesFile(dataRoot + os.sep + "inputChanges.csv", currentInputChanges)
 
 # If the user has specified a "copy in" folder, copy the contens of that folder over to the destination as well.
 # This happens after "scan folders", so for any conflicting filenames, the copy process will take precedence.
