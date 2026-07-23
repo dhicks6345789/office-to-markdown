ac856df6e73f5ac9eef55e1b732f54300f72bd9e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-23 15:19:38 +0100

    Update processSlideshow.py

--- slideshow/processSlideshow.py
+++ slideshow/processSlideshow.py
@@ -144,7 +144,13 @@ for inputPath in args["input"].iterdir():
         slideCount = slideCount + 1
 
 # Copy the "index.html" single-page, self-contained slideshow viewer to the output folder, adding in a list of the slides generated.
-officeToMarkdownLib.putFile(args["output"] / pathlib.Path("index.html"), officeToMarkdownLib.getFile("slideshow/slideshowIndex.html").replace("var resources = [];", str("var resources = " + str(slideList) + ";")))
+indexFileInputPath = pathlib.Path.cwd() / pathlib.Path("slideshow/slideshowIndex.html")
+indexFileInputPathStr = str(indexFileInputPath)
+indexFileInputPathStat = indexFileInputPath.stat()
+indexFileOutputPath = args["output"] / pathlib.Path("index.html")
+if scriptUpdated or (not indexFileOutputPath.is_file()) or (not indexFileInputPathStr in previousInputFileTimestamps) or (not str(inputPathStat.st_mtime) == previousInputFileTimestamps[indexFileInputPathStr]):
+    officeToMarkdownLib.putFile(indexFilePath, officeToMarkdownLib.getFile(indexFileInputPath).replace("var resources = [];", str("var resources = " + str(slideList) + ";")))
+filesProcessed[indexFileInputPathStr] = (str(indexFileOutputPath), str(indexFileInputPathStat.st_mtime))
 
 # Report the input filenames, with current update timestamp, back to the calling script, along with the output filenames.
 officeToMarkdownLib.printFilesProcessed(filesProcessed)
