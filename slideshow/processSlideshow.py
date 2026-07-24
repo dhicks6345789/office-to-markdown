0118bf0d1da43e581f969872e482e510f2b69f14
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-24 16:51:01 +0100

    Update processSlideshow.py

--- slideshow/processSlideshow.py
+++ slideshow/processSlideshow.py
@@ -32,6 +32,8 @@ args = vars(officeToMarkdownLib.setArgsForSubScript(argparse.ArgumentParser(desc
 # Pick up any additional arguments from a config file if present.
 args.update(officeToMarkdownLib.processArgsFile(args["input"], defaultArgs={"width":1024, "height":768}))
 
+ print(args, flush=True, file=sys.stderr)
+
 # The calling script provides a list of any input files, along with last-modified timestamps, via stdin as simple set of comma-separated "filename,timestamp" values.
 previousInputFileTimestamps = officeToMarkdownLib.readInputFilesAndTimestamps()
 
