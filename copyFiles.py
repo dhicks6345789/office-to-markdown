a4205f16752925e8f8b7b73331eafee2e7aab0eb
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-27 16:37:16 +0100

    Update copyFiles.py

--- copyFiles.py
+++ copyFiles.py
@@ -31,7 +31,7 @@ scriptUpdated = officeToMarkdownLib.checkIfScriptUpdated(previousInputFileTimest
 filesProcessed = {}
 def copyFiles(theInputPath, theOutputPath):
   if theInputPath.is_file():
-    outputFilePath =  args["outputRoot"] / officeToMarkdownLib.pathSlugify(theOutputPath / theInputPath.name)
+    outputFilePath =  args["outputRoot"] / officeToMarkdownLib.filePathSlugify(theOutputPath / theInputPath.name)
     inputPathStr = str(theInputPath)
     inputPathStat = theInputPath.stat()
     if scriptUpdated or (not outputFilePath.is_file()) or (not inputPathStr in previousInputFileTimestamps) or (not str(inputPathStat.st_mtime) == previousInputFileTimestamps[inputPathStr]):
