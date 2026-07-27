15a189ccd59e3d3d8a602836324310d43e06df2e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-27 16:28:09 +0100

    Update officeToMarkdownLib.py

--- officeToMarkdownLib.py
+++ officeToMarkdownLib.py
@@ -16,9 +16,6 @@ import PIL.Image
 # The ffmpeg video-handling library.
 import ffmpeg
 
-# The Slugify library for making URL-safe strings.
-import slugify
-
 # Mammoth converts .DOCX file to HTML...
 import mammoth
 # ...and Markdownify can convert HTML to Markdown.
@@ -548,8 +545,7 @@ def scanFolder(verbose, theScriptRoot, theMatches, theMatchTimestamps, thePrevio
                 inputTimestamp = "0"
                 if str(item) in thePreviousInputFileTimestamps:
                     inputTimestamp = thePreviousInputFileTimestamps[str(item)]
-                #commandLine = [scriptExec, scriptPathStr, "--scriptTimestamp", scriptTimestamp, "--input", str(item), "--outputRoot", str(theOutputRoot), "--output", str(theOutputFolder) + os.sep + slugify.slugify(item.name)]
-                commandLine = [scriptExec, scriptPathStr, "--input", str(item), "--outputRoot", str(theOutputRoot), "--output", str(theOutputFolder) + os.sep + slugify.slugify(item.name)]
+                commandLine = [scriptExec, scriptPathStr, "--input", str(item), "--outputRoot", str(theOutputRoot), "--output", str(theOutputFolder) + os.sep + item.name]
                 if verbose:
                     commandLine.append("--verbose")
                 matchInputItems = {}
@@ -607,7 +603,7 @@ def scanFolder(verbose, theScriptRoot, theMatches, theMatchTimestamps, thePrevio
             for matchInputItem in thePreviousInputFileTimestamps:
                 if matchInputItem.startswith(str(item)):
                     matchInputItems[matchInputItem] = thePreviousInputFileTimestamps[matchInputItem]
-            subNewInputFileTimestamps, subOutputFiles = scanFolder(verbose, theScriptRoot, theMatches, theMatchTimestamps, matchInputItems, item, theOutputRoot, theOutputFolder / pathlib.Path(slugify.slugify(item.name)))
+            subNewInputFileTimestamps, subOutputFiles = scanFolder(verbose, theScriptRoot, theMatches, theMatchTimestamps, matchInputItems, item, theOutputRoot, theOutputFolder / pathlib.Path(item.name))
             newInputFileTimestamps.update(subNewInputFileTimestamps)
             outputFiles.extend(subOutputFiles)
     return newInputFileTimestamps, outputFiles
