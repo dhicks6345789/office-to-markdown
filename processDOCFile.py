be8c49a94c9a2357c6c3774195b9fcbfdfc9e8bf
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-28 16:04:20 +0100

    Update processDOCFile.py

--- processDOCFile.py
+++ processDOCFile.py
@@ -34,7 +34,7 @@ def processFiles(theInputPath, theOutputPath):
     inputPathStat = theInputPath.stat()
     inputPathSuffix = theInputPath.suffix.lower()
     if inputPathSuffix in [".docx"]:
-      outputFilePath = args["outputRoot"] / theOutputPath.parent / pathlib.Path(theInputPath.stem + ".md")
+      outputFilePath = args["outputRoot"] / theOutputPath / pathlib.Path(theInputPath.stem + ".md")
       if scriptUpdated or (not outputFilePath.is_file()) or (not inputPathStr in previousInputFileTimestamps) or (not str(inputPathStat.st_mtime) == previousInputFileTimestamps[inputPathStr]):
         officeToMarkdownLib.ifVerbose(args["verbose"], "processDOCFile   -   " + inputPathSuffix + ": " + inputPathStr + " to " + str(outputFilePath))
         
