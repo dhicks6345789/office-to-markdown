81dee80b056e96017530b2091c76259d02dff218
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-07-28 17:45:31 +0100

    Update scanFolders.py

--- scanFolders.py
+++ scanFolders.py
@@ -151,6 +151,7 @@ officeToMarkdownLib.writeChangesFile(inputChangesPath, currentInputChanges)
 def copyFolder(inputFolder, outputFolder):
     for inputItem in inputFolder.iterdir():
         if not inputItem.name in officeToMarkdownLib.fileIgnores:
+            print("copy - " + inputItem)
             outputItem = outputFolder / pathlib.Path(inputItem.name)
             outputFiles.append(str(outputItem))
             if inputItem.is_file():
